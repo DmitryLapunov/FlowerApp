50cf733b8b58ea17788243dd0c0d507da09bde41
Yauheni <86980476+YauheniZhurauski@users.noreply.github.com>
2021-11-16 03:32:10 +0300

    openApp

---
+++ FlowerApp/FlowerApp/Cells/ContactsCell/ContactsCell.swift
@@ -0,0 +1,58 @@
+//
+//  ContactsCell.swift
+//  FlowerApp
+//
+//  Created by Евгений on 16.11.21.
+//
+
+import UIKit
+
+enum Application: String {
+    case telegram = "Telegram"
+    case instagram = "Instagram"
+    case facebook = "Facebook"
+    case viber = "+375(29)817-49-83"
+    case vk = "Вконтакте"
+    case phone
+    case googleMap
+}
+
+class ContactsCell: UITableViewCell {
+
+
+    @IBOutlet weak var imageCell: UIImageView!
+    @IBOutlet weak var textCell: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+
+func openApplication(application: Application) {
+    switch application {
+    case .facebook:
+        print("Facebook")
+    case .instagram:
+        let application = UIApplication.shared
+        let appURL = URL(string: "instagram://user?username=zhorawow")!
+        if application.canOpenURL(appURL) {
+            application.open(appURL, options: [:], completionHandler: nil)
+        } else {
+            let webURL = URL(string: "https://instagram.com/zhorawow/")!
+                    application.open(webURL)
+        }
+        
+        print("insta")
+    case .telegram:
+        print("telega")
+    case .vk:
+        print("vk")
+    case .viber:
+        print("viber")
+    case .googleMap:
+        print("google")
+    case .phone:
+        print("call")
+    }
+}
+}
