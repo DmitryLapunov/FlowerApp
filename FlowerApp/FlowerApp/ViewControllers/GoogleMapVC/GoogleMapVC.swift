01cd6dfd2b128a34b07affe673a70f9bb090b582
Yauheni <86980476+YauheniZhurauski@users.noreply.github.com>
2021-12-25 01:33:11 +0300

    FixMap

--- FlowerApp/FlowerApp/ViewControllers/GoogleMapVC/GoogleMapVC.swift
+++ FlowerApp/FlowerApp/ViewControllers/GoogleMapVC/GoogleMapVC.swift
@@ -13,52 +13,31 @@ class GoogleMapVC: UIViewController {
     @IBOutlet weak var mapView: GMSMapView!
     @IBOutlet weak var makeTripButton: UIButton!
     
-    var installedNavigationApps : [String] = ["Apple Maps"]
+    
+    var isYandexEnabled: Bool {
+        return UIApplication.shared.canOpenURL(URL(string: "yandexmaps://")!)
+    }
+    
+    var isGoogleEnabled: Bool {
+        return UIApplication.shared.canOpenURL(URL(string: "comgooglemaps://")!)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.isMyLocationEnabled = true
         createPin()
         correctButton()
-        checkApplication()
-    }
     
-    func checkApplication() {
-        let application = UIApplication.shared
-        
-        if application.canOpenURL(URL(string: "comgooglemaps://")!) {
-            self.installedNavigationApps.append("Google Maps")
-        }
-        
-        if application.canOpenURL(URL(string: "yandexmaps://")!) {
-            self.installedNavigationApps.append("Яндекс.Карты")
-        }
     }
     
     @IBAction func makeTripAction(_ sender: Any) {
         
         let application = UIApplication.shared
         let alert = UIAlertController(title: "Выбор", message: "Выберете приложение", preferredStyle: .actionSheet)
-        for app in self.installedNavigationApps {
-            let button = UIAlertAction(title: app, style: .default, handler: { buttonAction in
-            
-                switch app {
-                case "Apple Maps":
-                    let appURLApple = URL(string: "maps://?daddr=53.913147,27.593101")!
-                    if application.canOpenURL(appURLApple) {
-                        application.open(appURLApple, options: [:], completionHandler: nil)
-                    }
-                    
-                case "Google Maps":
-                    let appURLGoogle = URL(string: "comgooglemaps://?saddr=&daddr=53.913147,27.593101&directionsmode=driving")!
-                    if application.canOpenURL(appURLGoogle) {
-                        application.open(appURLGoogle, options: [:], completionHandler: nil)
-                    } else {
-                        let webURLGoogle = URL(string: "https://www.google.co.in/maps/dir/?saddr=&daddr=53.913147,27.593101&directionsmode=driving")!
-                        application.open(webURLGoogle)
-                    }
-                    
-                case "Яндекс.Карты":
+        
+        if isYandexEnabled || isGoogleEnabled {
+            if isYandexEnabled {
+                let yandexButton = UIAlertAction(title: "Яндекс.Карты", style: .default) { yandex in
                     let appURLYandex = URL(string: "yandexmaps://build_route_on_map/?lat_to=53.913147&lon_to=27.593101")!
                     if application.canOpenURL(appURLYandex) {
                         application.open(appURLYandex, options: [:], completionHandler: nil)
@@ -66,20 +45,34 @@ class GoogleMapVC: UIViewController {
                         let webURLYandex = URL(string: "https://yandex.ru/maps/?rtext=~53.913147,27.593101&rtt=auto")!
                         application.open(webURLYandex)
                     }
-                    
-                default:
-                    print("Error")
                 }
-            })
-            alert.addAction(button)
+                alert.addAction(yandexButton)
+            }
+            
+            if isGoogleEnabled {
+                let googleButton = UIAlertAction(title: "Google Maps", style: .default) { google in
+                    let appURLGoogle = URL(string: "comgooglemaps://?saddr=&daddr=53.913147,27.593101&directionsmode=driving")!
+                    if application.canOpenURL(appURLGoogle) {
+                        application.open(appURLGoogle, options: [:], completionHandler: nil)
+                    } else {
+                        let webURLGoogle = URL(string: "https://www.google.co.in/maps/dir/?saddr=&daddr=53.913147,27.593101&directionsmode=driving")!
+                        application.open(webURLGoogle)
+                    }
+                }
+                alert.addAction(googleButton)
+            }
+            
+            let cancelButton = UIAlertAction(title: "Отмена", style: .cancel) { cancel in
+                self.dismiss(animated: true, completion: nil)
+            }
+            alert.addAction(cancelButton)
+
+            present(alert, animated: true, completion: nil)
+            
+        } else {
+            let webURLGoogle = URL(string: "https://www.google.co.in/maps/dir/?saddr=&daddr=53.913147,27.593101&directionsmode=driving")!
+            application.open(webURLGoogle)
         }
-        
-        let cancelButton = UIAlertAction(title: "Отмена", style: .cancel) { cancel in
-            self.dismiss(animated: true, completion: nil)
-        }
-        alert.addAction(cancelButton)
-        
-        present(alert, animated: true, completion: nil)
     }
     
     func correctButton() {
